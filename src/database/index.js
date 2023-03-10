52f79c35b7d0191b3ee43096a3bdd6fd2125b995
Lucas Felix <lucasfelixdev@gmail.com>
2023-03-10 19:35:31 -0300

    chore: db connection and repository refactor

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const { Client } = require('pg');
+
+const client = new Client({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
+
+client.connect();
+
+exports.query = async (query, values) => {
+  const { rows } = await client.query(query, values);
+
+  return rows;
+};
